2225ccdd2d167f29ace8126d6810e0da80d56179
Tony Pelaez <tnyplz@gmail.com>
2009-07-06 19:29:02 -0700

    allowed new users to register by skipping the check_privacy method for the new and create user methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   
   before_filter :can_edit, :only => [:edit, :update, :destroy, :confirm_delete]
   before_filter :require_admin, :only => [:admin, :ban, :remove_ban]
-  skip_filter :check_privacy, :only => [:login, :logout]
+  skip_filter :check_privacy, :only => [:login, :logout, :new, :create]
   
   def index
     @users = User.paginate(:page => params[:page], :order => 'profile_updated_at desc')
